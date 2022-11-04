08f05ae3df779087ffae7eda1c5aaf907b1a8673
mario.sanchez.interacpedia <mario.sanchez@talentpitch.co>
2022-11-03 21:47:20 -0500

    add components

---
+++ src/app/modulos/encomiendas/encomiendas.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EncomiendasRoutingModule } from './encomiendas-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    EncomiendasRoutingModule
+  ]
+})
+export class EncomiendasModule { }
