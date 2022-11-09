779780cda68de0b248e94b327366648bc5f7023a
mario.sanchez.interacpedia <mario.sanchez@talentpitch.co>
2022-11-08 20:23:34 -0500

    login form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,24 @@ const routes: Routes = [
     component: IndexComponent,
   },
   {
+    path: 'seguridad',
+    loadChildren: () => import('./modulos/seguridad/seguridad.module').then(m => m.SeguridadModule)
+  },{
+    path: 'admin',
+    loadChildren: () => import('./modulos/admin/admin.module').then(m => m.AdminModule)
+  },{
+    path: 'clientes',
+    loadChildren: () => import('./modulos/clientes/clientes.module').then(m => m.ClientesModule)
+  },
+  {
+    path: 'encomiendas',
+    loadChildren: () => import('./modulos/encomiendas/encomiendas.module').then(m => m.EncomiendasModule)
+  },
+  {
+    path: 'servicios',
+    loadChildren: () => import('./modulos/servicios/servicios.module').then(m => m.ServiciosModule)
+  },
+  {
     path: '',
     pathMatch: 'full',
     redirectTo: '/index'
